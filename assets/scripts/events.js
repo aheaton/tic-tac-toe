6e53e38d7a7bba559f0e1a1ec4d8637f59a3f0e6
aheaton <Pappy123!>
2017-09-26 20:16:28 -0400

    Add squareClick function to put x's in cells when clicked

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,3 +1,21 @@
 // const gameApi = require('./api.js')
 // const gameUi = require('./ui.js')
 // const getFormFields = require('../../../lib/get-form-fields')
+
+const player = 'x'
+const squareClick = function (event) {
+  const square = event.target
+  console.log(square)
+  if ($(square).text() === 'x' || $(square).text() === 'o') {
+    return null
+  } else {
+  // puts either X or O in the square that is clicked and this is dependent on what "player" is set to
+    $(square).text(player)
+    // callback function which switches the "player" to either X or O conditionally
+    // playerSwitch()
+  }
+}
+
+module.exports = {
+  squareClick
+}
