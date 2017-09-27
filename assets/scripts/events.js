e0cdf895bad660993f90e3b1400a0e70066d8dcd
aheaton <Pappy123!>
2017-09-27 13:09:02 -0400

    Fix bug - could keep clicking cells when game over

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -5,12 +5,16 @@ const gameLogic = require('./logic.js')
 
 const squareClick = function (event) {
   const square = event.target
-  console.log(square)
   if ($(square).text() === 'x' || $(square).text() === 'o') {
     return null
   } else {
+    // checks if you can keep playing the game depending on if game is already over
+    if (gameLogic.gameOver() === true) {
+      return null
+    } else {
     // the following function initializes the player at game start and switches players
-    gameLogic.playerSwitch(square)
+      gameLogic.playerSwitch(square)
+    }
   }
 }
 
--- assets/scripts/logic.js
+++ assets/scripts/logic.js
@@ -12,27 +12,27 @@ const winCases = [
   [2, 4, 6]
 ]
 
+// an array to stores x's moves
 const xMoves = []
 const xMovesStore = function (square) {
   // get id from clicked cell and push to an array
   const squareNumber = $(square).attr('id')
-  console.log('square number ', squareNumber)
   // parseInt turns the squareNumber from a string to a number
   xMoves.push(parseInt(squareNumber))
   return xMoves
 }
 
+// an array to stores o's moves
 const oMoves = []
 const oMovesStore = function (square) {
-  console.log('square is ', square)
   // get id from clicked cell and push to an array
   const squareNumber = $(square).attr('id')
-  console.log('square number ', squareNumber)
   oMoves.push(parseInt(squareNumber))
   return oMoves
 }
 
 // this function compares the winCases array with the appropriate moves array and lays out win logic
+let over = false
 const gameStatus = function (winCases, moves) {
   for (let i = 0; i < winCases.length; i++) {
     // want to be checking each sub-array of winCases
@@ -40,20 +40,22 @@ const gameStatus = function (winCases, moves) {
     // compare the xMoves or oMoves array with each winCases sub-array and put into common array
     const common = winCasesSub.filter(cell => moves.includes(cell))
     // checks for winner (i.e. 3 matching cells with the winCases sub-arrays)
-    console.log(common)
     if (common.length === 3 && moves === xMoves) {
+      over = true
       return gameUi.xWins()
     } else if (common.length === 3 && moves === oMoves) {
+      over = true
       return gameUi.oWins()
     }
   }
 }
 if (xMoves.length + oMoves.length === 9) {
+  over = true
   gameUi.tie()
 }
 
-// this function adds the moves function to add to the moves arrays and also checks for a win
-const checkWin = function (square) {
+// this function adds the moves function to add to the moves arrays and also checks game status
+const checkGameStatus = function (square) {
   if (player === 'x') {
     xMovesStore(square)
     // put array comparison function here
@@ -65,11 +67,19 @@ const checkWin = function (square) {
   }
 }
 
+// passed to the squareClick function to see if the game should continue before switching to next player
+const gameOver = function () {
+  if (over === true) {
+    return true
+  }
+}
+
+// if no win
 let player = 'x'
 const playerSwitch = function (square) {
   // initalizes first player to x based on global variable
   $(square).text(player)
-  checkWin(square)
+  checkGameStatus(square)
   if (player === 'x') {
     player = 'o'
   } else {
@@ -81,6 +91,7 @@ module.exports = {
   playerSwitch,
   xMovesStore,
   oMovesStore,
-  checkWin,
-  gameStatus
+  checkGameStatus,
+  gameStatus,
+  gameOver
 }
